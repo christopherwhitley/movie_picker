faa830faa8914d136ae000b18f288ee35f68cc13
Chris Whitley <chris_whitley@hotmail.co.uk>
2021-10-22 17:04:50 +0100

    Added test for deleting person

--- test/models/person_test.rb
+++ test/models/person_test.rb
@@ -6,9 +6,22 @@ class PersonTest < ActiveSupport::TestCase
   # end
   test "person_count" do
     assert_equal 2, Person.count
-    user5 = create(:person, :password => "123")
-    assert_equal 3, Person.count
-    user6 = create(:person, :password => "987")
-    Rails::logger.debug Person.pluck(:email)
   end
+
+  test "person_factory" do
+    user5 = create(:person, :password => "123")
+    user6 = create(:person, :password => "987")
+    assert_equal 4, Person.count
+  end
+
+  test "user_delete" do
+    user5 = create(:person, :password => "123")
+    user6 = create(:person, :password => "987")
+    Person.first.destroy()
+    assert_equal 3, Person.count
+  end
+
+
+#Rails::logger.debug Person.pluck(:email)
+
 end
