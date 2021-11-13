fe93820fb5dabdfd6d529f7b56e9c411475b0fc8
Chris Whitley <chris_whitley@hotmail.co.uk>
2021-11-13 11:03:17 +0000

    Show and hide content with button on show film page

---
+++ app/javascript/packs/test.js
@@ -0,0 +1,30 @@
+window.onload=function(){
+
+  button = document.getElementById('show-hide');
+  itemsToHide = [...document.querySelectorAll('.release')].forEach(function(item) {
+      item.style.display = 'none'
+    });
+
+
+  button.addEventListener('click', function() {
+
+    itemsToHide = [...document.querySelectorAll('.release')].forEach(function(item) {
+        if(item.style.display === 'none') {
+          item.style.display = 'block';
+        }
+        else {
+          item.style.display = 'none'
+        }
+      });
+
+    });
+}
+
+
+
+
+//function finishFunction() {
+//  alert ("Test message");
+//}
+
+//finishFunction();
