789525fe0a112537c7af2785798aec0fda578597
Chris Whitley <chris_whitley@hotmail.co.uk>
2021-11-15 16:54:17 +0000

    Remove film_id from person function

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -18,3 +18,22 @@ class ApplicationController < ActionController::Base
     redirect_to '/pages/login' unless logged_in?
   end
 end
+
+def add_film_to_person(film_id)
+  respond_to do |format|
+    if logged_in?
+      format.html { redirect_to people_path }
+      current_user.add_film_id_to_person(film_id)
+  else
+    format.html { redirect_to people_url, notice: "Need to login." }
+  end
+end
+
+def remove_film_id_from_person(id)
+  respond_to do |format|
+    format.html { redirect_to people_url, notice: "Film removed from person"}
+    current_user.remove_film_id_from_person(id)
+  end
+end
+
+end
