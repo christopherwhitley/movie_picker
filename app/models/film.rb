88bc8996c42e1c6315a0ce7474f244f53b5660a1
Chris Whitley <chris_whitley@hotmail.co.uk>
2021-11-27 19:24:09 +0000

    Added method for returning film_objects with genre match

--- app/models/film.rb
+++ app/models/film.rb
@@ -6,21 +6,30 @@ require 'api.rb'
   has_and_belongs_to_many :people
   validates :title, presence: true, uniqueness: true
 
+@allFilms = nil
+
 def recommended_films
-    allFilms = Film.all
+    @allFilms = Film.all
     api = ApiCall.new
     @myapi = []
-    #@titles = []
-    film_genre_match = allFilms.select { |movie| movie.genre_id == genre_id }
+    @titles = []
+    film_genre_match = @allFilms.select { |movie| movie.genre_id == genre_id }
     #recommended_results = nil
     film_genre_match.each do |film|
       results = api.api_call(film.title)
-      titles = results.find { |movie| movie['title'] }
+      titles = results.find { |movie| movie['title'] == film.title && movie['original_language'] == 'en' }
       @myapi << titles
+
       puts 'execute loop'
     end
 
     return @myapi
+end
+
+def recommended_films_ids
+  allfilms = Film.all
+  film_genre_match = @allFilms.select { |movie| movie.genre_id == genre_id }
+  return film_genre_match
 end
 
 end
