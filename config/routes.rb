9e8e587e793d81bb6cc0f96360b1c9928f93138f
Chris Whitley <chris_whitley@hotmail.co.uk>
2021-11-27 11:55:38 +0000

    Formatting

--- config/routes.rb
+++ config/routes.rb
@@ -1,21 +1,29 @@
 Rails.application.routes.draw do
-  resources :people
   root to: 'pages#home'
-  get '/randomfilm' => 'films#rand'
 
+# Films
   resources :films, except: :create
   post "/films" => "films#multiple_film_results", :as => 'filmsubmission'
   post "/films/new" => "films#create", :as =>'new_film_path'
-  resources :genres
+  get 'films/confirmation'
+  get '/randomfilm' => 'films#rand'
+
+
+# Pages
+  get '/api' => 'pages#api'
   get 'pages/login'
   get 'pages/logout'
-  get 'films/confirmation'
   post 'pages/login'
   post 'pages/logout'
   delete 'pages/logout' => 'pages#destroy'
 
-post 'people_path' => 'people#add_film_to_person', as: 'add_film_to_person'
+# Genres
+  resources :genres
+
+# People
+  post 'people_path' => 'people#add_film_to_person', as: 'add_film_to_person'
+  resources :people
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  get '/api' => 'pages#api'
+
 end
