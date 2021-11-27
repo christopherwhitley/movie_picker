3b5668b8e33b007c368e8cc1f71cd26bf26f6cda
Chris Whitley <chris_whitley@hotmail.co.uk>
2021-11-27 11:55:54 +0000

    Added api call to lib folder in its own class

---
+++ lib/api.rb
@@ -0,0 +1,16 @@
+class ApiCall
+
+
+def api_call(title)
+
+  query = {"query" => title}
+  response = HTTParty.get("https://api.themoviedb.org/3/search/movie?api_key=b6ba0af499c6872471a982365c647f0e&language=en-US",
+    :query => query,
+    format: :json)
+    response = response.parsed_response
+    puts 'api triggered'
+    results = response["results"]
+end
+
+
+end
