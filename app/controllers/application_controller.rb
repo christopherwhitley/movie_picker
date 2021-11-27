e31ae133f0f852f3d090ce6e7f89f9ff3c9dc6ef
Chris Whitley <chris_whitley@hotmail.co.uk>
2021-11-27 11:51:36 +0000

    Added an api_call method

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,6 +5,7 @@ class ApplicationController < ActionController::Base
   before_action :authorized
   helper_method :current_user
   helper_method :logged_in?
+  helper_method :api_call
 
   def current_user
     Person.find_by(id: session[:user_id])
@@ -16,6 +17,16 @@ class ApplicationController < ActionController::Base
 
   def authorized
     redirect_to '/pages/login' unless logged_in?
+  end
+
+  def api_call(title)
+
+    query = {"query" => title}
+    @response = HTTParty.get("https://api.themoviedb.org/3/search/movie?api_key=b6ba0af499c6872471a982365c647f0e&language=en-US",
+      :query => query,
+      format: :json)
+      response = @response.parsed_response
+      @results = response["results"]
   end
 end
 
@@ -35,5 +46,6 @@ def remove_film_id_from_person(id)
     current_user.remove_film_id_from_person(id)
   end
 end
+
 
 end
