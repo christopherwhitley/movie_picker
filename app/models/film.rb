283c7066a3359a8c386329c2107e69d496e5f50e
Chris Whitley <chris_whitley@hotmail.co.uk>
2021-11-28 17:55:34 +0000

    Prevent same film being recommended as being shown

--- app/models/film.rb
+++ app/models/film.rb
@@ -16,7 +16,7 @@ def recommended_films
     film_genre_match = @allFilms.select { |movie| movie.genre_id == genre_id }
     film_genre_match.each do |film|
       results = api.api_call(film.title)
-      titles = results.find { |movie| movie['title'] }
+      titles = results.find { |movie| movie['title'] == film.title }
       @myapi << titles
 
     end
@@ -31,19 +31,13 @@ end
 
 def get_recommended_film_poster(film_name, response)
   #Returns an array of Film objects
-
   @results = []
   @poster = nil
+
   #For each film object in the array, find, in our api_call results, the first result that matches the title in the results with the title of our film object
-
     result = response.find { |movie| movie['title'] == film_name }
-
     @poster = result['poster_path']
-
     return @poster
   end
-
-
-
 
 end
