ab11c5a7ffba98b4064a2ae46a8e6d9da7e2ce69
Chris Whitley <chris_whitley@hotmail.co.uk>
2021-12-01 18:02:42 +0000

    Added ability to view full size poster on click to show-films page

--- app/javascript/packs/test.js
+++ app/javascript/packs/test.js
@@ -1,13 +1,21 @@
+var overlay = document.getElementById("overlay")
+var poster = document.getElementById("poster")
 
+// Display full size poster on clicking poster
+poster.addEventListener('click', function() {
+    overlay.style.display = "block";
+  });
+// Hide full size poster on click
+overlay.addEventListener('click', function() {
+      overlay.style.display = "none";
+    });
 
+// Show / hide film details
   button = document.getElementById('show-hide');
   itemsToHide = [...document.querySelectorAll('.release')].forEach(function(item) {
       item.style.display = 'none'
     });
-
-
   button.addEventListener('click', function() {
-
     itemsToHide = [...document.querySelectorAll('.release')].forEach(function(item) {
         if(item.style.display === 'none') {
           item.style.display = 'block';
@@ -16,7 +24,6 @@
           item.style.display = 'none'
         }
       });
-
     });
 
 
