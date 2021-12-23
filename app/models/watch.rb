fdd1d8ec7b4d6e3365706dc87a9569784f65ad42
Chris Whitley <chris_whitley@hotmail.co.uk>
2021-12-23 12:55:22 +0000

    Added checkmark if film is watched

--- app/models/watch.rb
+++ app/models/watch.rb
@@ -1,3 +1,9 @@
 class Watch < ApplicationRecord
   validates :watched_film_id, uniqueness: {scope: :watched_person_id}
+
+
+  def self.film_watched(person_id, film_id)
+    watched = Watch.where(:watched_person_id => person_id, :watched_film_id => film_id)
+    watched.pluck(:watched).pop
+  end
 end
