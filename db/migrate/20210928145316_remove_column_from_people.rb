23c82aa22002336f2019431af8c132e7c3f98f90
Chris Whitley <chris.whitley@futurelearn.com>
2022-11-06 11:43:36 +0000

    Push production credentials

--- db/migrate/20210928145316_remove_column_from_people.rb
+++
@@ -1,5 +0,0 @@
-class RemoveColumnFromPeople < ActiveRecord::Migration[6.1]
-  def change
-    remove_column :people, :film_id
-  end
-end
--- db/migrate/20211002102140_remove_film_id_from_people.rb
+++
@@ -1,5 +0,0 @@
-class RemoveFilmIdFromPeople < ActiveRecord::Migration[6.1]
-  def change
-    remove_column :people, :film_id, :text
-  end
-end
