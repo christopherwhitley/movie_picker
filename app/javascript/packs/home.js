2f0f587a9430ab655aa5f9938f2fed6b19ee7b86
Chris Whitley <chris.whitley@futurelearn.com>
2022-11-21 14:09:56 +0000

    Update homepage and faq styling

--- app/javascript/packs/home.js
+++ app/javascript/packs/home.js
@@ -1,26 +1,25 @@
-window.onload=function(){
+window.onload = function () {
 
 
-  [...document.querySelectorAll('.home-button')].forEach(function(item) {
-    item.addEventListener('mouseenter', function() {
+  [...document.querySelectorAll('.home-button')].forEach(function (item) {
+    item.addEventListener('mouseenter', function () {
       this.style.background = "#9DFED7";
     });
-    item.addEventListener('mouseleave', function() {
+    item.addEventListener('mouseleave', function () {
       this.style.background = "#EAFBF4";
-     });
-     });
-
-
-const btns = document.querySelectorAll(".question-btn");
-
-btns.forEach(function (btn) {
-  btn.addEventListener("click", function (e) {
-    const question = e.currentTarget.parentElement.parentElement
-    question.classList.toggle ("show-text")
-    //console.log("hello");
-      //console.log(e.currentTarget.parentElement.parentElement);
-      ;
+    });
   });
-});
+
+
+  const btns = document.querySelectorAll(".question-btn");
+
+  btns.forEach(function (btn) {
+    btn.addEventListener("click", function (e) {
+      const question = e.currentTarget.parentElement.parentElement
+      question.classList.toggle("show-text")
+      //console.log("hello");
+      //console.log(e.currentTarget.parentElement.parentElement);
+    });
+  });
 
 }
