f76266502b3e3d554482b350b31f7f668947d7ee
Chris Whitley <chris.whitley@futurelearn.com>
2022-11-25 15:52:19 +0000

    Improve various things in person model

--- app/models/person.rb
+++ app/models/person.rb
@@ -1,13 +1,9 @@
 class Person < ApplicationRecord
   has_many :films
-  serialize :film_id, Array
-
-  after_initialize do |person|
-      person.film_id = [] if person.film_id == nil
-    end
+  has_many :film_ids
 
   has_secure_password
-  validates :password, length: { in: 8..20 }, presence: true
+  validates :password, length: { in: 8..20 }, presence: true, on: :create
   validates :username, :email, uniqueness: true
 
 #Method of a Class
@@ -40,12 +36,16 @@ class Person < ApplicationRecord
     end
   end
 
-  def add_film_id_to_person(id)
-    if film_id.exclude? id
-        film_id << (id.to_s)
-        save
+  def add_film_id_to_person(person, id)
+    puts person.valid?
+    puts person.errors.messages
+    if person.film_id.exclude? id
+        puts person.film_id
+        person.film_id << id
+        person.save!
     else
-        return
+        flash.alert = 'Film not saved'
+        puts person.film_id
     end
   end
 
