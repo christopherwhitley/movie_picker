b3674151e6e474ecd6421110a81e7bee9f8ceab7
christopherwhitley <chris.whitley@futurelearn.com>
2022-12-18 16:58:43 +0000

    Add method for finding watched film

--- app/models/watch.rb
+++ app/models/watch.rb
@@ -1,9 +1,19 @@
 class Watch < ApplicationRecord
   validates :film_id, uniqueness: {scope: :person_id}
 
-
   def self.film_watched(person_id, film_id)
     watched = Watch.where(:person_id => person_id, :film_id => film_id)
     watched.pluck(:watches).pop
+  end
+
+  def self.find_watched_film(film_id)
+    thing = Watch.find_by(film_id: film_id)
+    if thing != nil
+      puts 'true'
+      return true 
+    else
+      puts 'false'
+      return false
+    end
   end
 end
