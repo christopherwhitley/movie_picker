581b7159b4b23f01f3fb4cbb912316eca4640399
christopherwhitley <chris.whitley@futurelearn.com>
2023-01-09 16:05:43 +0000

    Add scrolling film banner to homepage

--- app/javascript/packs/home.js
+++ app/javascript/packs/home.js
@@ -1,5 +1,51 @@
-window.onload = function () {
 
+
+window.addEventListener('DOMContentLoaded', () => {
+  const banner = document.getElementById("banner");
+  const films = document.querySelectorAll('.film');
+  const bannerScrollWidth = banner.scrollWidth;
+
+  var refreshIntervalId = setInterval(function () {
+    if (banner.scrollLeft != bannerScrollWidth) {
+      banner.scrollTo(banner.scrollLeft + 1, 0);
+    }
+    if (banner.offsetWidth + banner.scrollLeft + 0.5 == banner.scrollWidth) {
+      console.log("start again")
+      banner.scrollTo(0, 0)
+      banner.scrollTo(banner.scrollLeft + 1, 0);
+    }
+  }, 15);
+
+  films.forEach((f) => {
+    f.addEventListener("mouseenter", stopScroll);
+  });
+
+  films.forEach((f) => {
+    f.addEventListener("mouseleave", continueScroll);
+  });
+
+  function stopScroll() {
+    console.log("stop")
+    clearInterval(refreshIntervalId);
+  };
+  function continueScroll() {
+    console.log("start")
+    setInterval(function () {
+      if (banner.scrollLeft != bannerScrollWidth) {
+        banner.scrollTo(banner.scrollLeft + 1, 0);
+      }
+      if (banner.offsetWidth + banner.scrollLeft + 0.5 == banner.scrollWidth) {
+        console.log("start again")
+        banner.scrollTo(0, 0)
+        banner.scrollTo(banner.scrollLeft + 1, 0);
+      }
+    }, 15);
+  };
+});
+
+
+
+window.onload = function () {
 
   [...document.querySelectorAll('.home-button')].forEach(function (item) {
     item.addEventListener('mouseenter', function () {
