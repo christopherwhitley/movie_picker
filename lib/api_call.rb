68cbdf65d11640bd9347c230fd310c3ba9f687f7
Chris Whitley <58173975+christopherwhitley@users.noreply.github.com>
2025-11-09 11:25:02 +0000

    Fix incorrect logging

--- lib/api_call.rb
+++ lib/api_call.rb
@@ -3,7 +3,7 @@ class ApiCall
     query = { "query" => title }
     response = HTTParty.get("https://api.themoviedb.org/3/search/movie?api_key=b6ba0af499c6872471a982365c647f0e", query: query, include_adult: false, format: :json)
     response = response.parsed_response
-    Rails.logger.info("API_RESPONSE", JSON.pretty_generate(response))
+    Rails.logger.info("API_RESPONSE:, #{JSON.pretty_generate(response)}")
     response["results"]
   end
 end
